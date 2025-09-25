01273eb7aa40da11733c3b28a7be418bbb5c7dab
e <sacramento.marcosf@gmail.com>
2025-09-25 18:02:53 -0300

    Refactor repository and server logic; add Fastify routes for user management and integrate Prisma client

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,32 @@
-import prisma from './repository/repository'
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import {todosUsuarios, insertUsuario} from './repository/repository'
+import { CreateUsuarioDTO, UsuarioDTO } from './types/userTypes'
 
 
-async function todosUsuarios(){
-    const dados = await prisma.tabela_teste.findMany()
-    return dados
-}
+const fastify = Fastify({
+    logger:true
+})
 
-todosUsuarios()
-    .then((dados) => console.log(dados))
-    .catch((e) => console.log('[ERRO] - ' + e))
+fastify.get('/usuarios', async (request, reply)=>{
+    const dados = await todosUsuarios()
+    console.log(dados)
+    reply.status(200).send(dados)
+})
+
+fastify.get('/usuario/:id', async (request:FastifyRequest<{Params: UsuarioDTO}>, reply)=>{
+    reply.status(200).send({mensagem:`usuario ${request.params.id}`})
+})
+
+fastify.post('/usuariocreate/:id', async (request:FastifyRequest<{Params: CreateUsuarioDTO}>, reply:FastifyReply)=>{
+    const id_usado = request.params.id
+    try {
+    const id_retornado = await insertUsuario(id_usado)
+    reply.status(200).send({mensagem:`usuario ${id_retornado} inserido com sucesso`})
+    } catch (e){
+        reply.status(409).send({message:e + '\n - Id inserido incorreto'})
+    }
+})
+
+fastify.listen({
+    port: 3000
+})
