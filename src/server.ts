bca329fbaa3866d5aea8692f1d09be5171cadf65
sacramentomarcos <sacramento.marcosf@gmail.com>
2025-09-27 00:11:45 -0300

    Refactor server setup to use Fastify and implement '/all_usuarios' endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import prisma from './repository/repository'
+import Fastify from 'fastify'
 
+const fastify = Fastify({
+    logger: true
+})
 
 async function todosUsuarios(){
     const dados = await prisma.tabela_teste.findMany()
     return dados
 }
 
-todosUsuarios()
-    .then((dados) => console.log(dados))
-    .catch((e) => console.log('[ERRO] - ' + e))
+fastify.get('/all_usuarios', async (request, reply) => {
+    const dados = await todosUsuarios()
+    reply.send({dados})
+})
+
+fastify.listen({
+    port: 3333
+})
